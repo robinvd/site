4b025b2c57495443bfa803b3da4c8a0aab8579df
Robin <robinvandijk@klippa.com>
2025-03-18 12:35:51 +0100

    clean up style

---
+++ public/code.js
@@ -0,0 +1,20 @@
+const html = document.querySelector('html');
+const colorSelector = document.querySelector('#mode-switcher');
+function switchMode(mode) {
+  localStorage.setItem("mode", mode);
+  html.style.setProperty("color-scheme", mode === "auto" ? "light dark" : mode);
+  colorSelector.value = mode
+}
+switchMode(localStorage.getItem("mode") || "auto");
+function prefetch(target) {
+  const href = target.href;
+  const link = document.createElement('link');
+  for (const node of document.head.children) {
+    if (node.href === href) {
+      return
+    }
+  }
+  link.rel = 'prefetch';
+  link.href = href;
+  document.head.appendChild(link);
+}
